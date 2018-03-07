5531cbc54e837b9e655b0e58daa8f27c75e462b4
Aymeric DIJOUX <aymericelie.dijoux@gmail.com>
2018-03-08 00:39:50 +0100

    trease hunter game modification package

--- server.js
+++ server.js
@@ -67,10 +67,10 @@ router.route('/treasure')
 
 
 app.use('/api', router);
-
+app.use('/node_modules', express.static(`${__dirname}/node_modules`));
+app.use('/assets', express.static(`${__dirname}/assets`));
 /*Lunch server*/
 app.listen(port, () => {
 	console.log(`Server is running ${port}`);
 });
-app.use('/node_modules', express.static(`${__dirname}/node_modules`));
-app.use('/assets', express.static(`${__dirname}/assets`));
+
